143481a3518479be7b715e0826d3b3895f49eae2
m-rivera1 <mike.11.mxr@gmail.com>
2021-02-04 16:37:54 -0600

    minor updates...just need to add panels for tool tips and change fonts

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,12 +31,14 @@ d3.csv('/assets/data/data.csv').then(function(demoData) {
         data.poverty = +data.poverty;
         data.healthcare = +data.healthcare;
         data.abbr = data.abbr;
+        data.state = data.state;
+        console.log(data.poverty, data.healthcare);
     });
 
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(demoData, d => d.poverty)])
+      .domain([8, d3.max(demoData, d => d.poverty)])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
@@ -66,7 +68,7 @@ d3.csv('/assets/data/data.csv').then(function(demoData) {
     .attr("cx", d => xLinearScale(d.poverty))
     .attr("cy", d => yLinearScale(d.healthcare))
     .attr("r", "15")
-    .attr("fill", "pink")
+    .attr("fill", "purple")
     .attr("opacity", ".5");
 
     // Step 6: Initialize tool tip
@@ -75,7 +77,7 @@ d3.csv('/assets/data/data.csv').then(function(demoData) {
       .attr("class", "tooltip")
       .offset([80, -60])
       .html(function(d) {
-        return (`${d.abbr}<br>Poverty: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
+        return (`${d.state}<br>In Poverty (%): ${d.poverty}<br>Lacks Healthcare (%): ${d.healthcare}`);
       });
 
     // Step 7: Create tooltip in the chart
@@ -99,12 +101,12 @@ d3.csv('/assets/data/data.csv').then(function(demoData) {
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em") // NOTE 'em' is 10px units and 'd'  is like a 'delta' up the y-axis in this case
       .attr("class", "axisText")
-      .text("Healthcare");
+      .text("Lacks Healthcare (%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Poverty");
+      .text("In Poverty (%)");
   }).catch(function(error) {
     console.log(error);
   });
